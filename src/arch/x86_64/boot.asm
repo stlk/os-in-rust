37839ce9f1ab44a7e07358eb234462d0979998ac
Josef Rousek <josef.rousek@gmail.com>
2018-04-07 14:47:17 +0200

    Multiboot

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -5,6 +5,7 @@ section .text
 bits 32
 start:
     mov esp, stack_top
+    mov edi, ebx       ; Move Multiboot info pointer to edi
 
     call check_multiboot
     call check_cpuid
@@ -198,5 +199,5 @@ p3_table:
 p2_table:
     resb 4096
 stack_bottom:
-    resb 64
+  resb 4096
 stack_top:
